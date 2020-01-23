cd1747e680c79faabb38d4b7a9f7b2c9a30e1c49
cmatchneer <cmatchneer@yahoo.com>
2020-01-22 19:09:59 -0500

    get paths are all up

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var PORT = 3000;
-var waitList = [];
-var heldTables = [];
+var waitList = [{
+    name: "Chris",
+    partySize: 5,
+    time: 3 + "pm",
+    id: 1
+}];
+var heldTables = [{
+    name: "Joe",
+    partySize: 2,
+    time: 7 + "pm",
+    id: 1
+}];
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,10 +23,18 @@ app.get("/", function(input, response) {
 
 app.get("/reserve", function(input, response) {
     response.sendFile(path.join(__dirname, "reserve.html"));
-})
+});
 app.get("/table", function(input, response) {
     response.sendFile(path.join(__dirname, "tableView.html"));
-})
+});
+app.get("/api/reserved", function(input, response) {
+    return response.json(heldTables);
+});
+app.get("/api/waiting", function(input, response) {
+    return response.json(waitList);
+});
+
+
 
 
 
